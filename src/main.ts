dce7cd0e69cf0b7448bc93085ce598fa821e28ed
haquocdat543 <wwwdatha543@gmail.com>
2024-11-06 22:00:19 +0700

    config: run at port 3000

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
+import { PORT } from "./constants";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
@@ -18,7 +19,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
-  await app.listen(6868);
+  await app.listen(PORT);
 }
 
 bootstrap();
